5335eaed90d6d53985a115b1f1ca4048b89fcbd2
esoteldo <soteldo18@hotmail.com>
2025-10-27 12:57:43 -0400

    logo added

--- server/src/config.js
+++ server/src/config.js
@@ -1,2 +1,2 @@
-export const  MONGODB_ATLAS='mongodb+srv://enriquesoteldodev1_db_user:QLC0vbbsaInyHmXb@cluster0.xedpnc6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+export const  MONGODB_ATLAS=''
 export const PORT =  3000;
